a289c122bd2f1bca728b80f05b241146077e7851
EnebeliEmmanuel <iemmaenebeli@gmail.com>
2022-07-19 16:31:41 -0700

    resolved

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useContext, useEffect } from "react";
-import "./App.css";
+import React , { useContext, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
   Box,
